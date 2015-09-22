d481bb5aa72938978570193151fbdc2139bc44ba
Rachel Popo <rach.popo@gmail.com>
2015-09-21 21:56:36 -0600

    Create Point.h

---
+++ Point.h
@@ -0,0 +1,63 @@
+//
+// Created by Rachel Popo on 9/21/15.
+//
+
+#ifndef CSI2312_PA2_POINT_H
+#define CSI2312_PA2_POINT_H
+
+#include <iostream>
+
+namespace Clustering {
+
+    class Point {
+        int dim;        // number of dimensions of the point
+        double *values; // values of the point's dimensions
+
+    public:
+        Point(int);
+        Point(int, double *);
+
+        // Big three: cpy ctor, overloaded operator=, dtor
+        Point(const Point &);
+        Point &operator=(const Point &);
+        ~Point();
+
+        // Accessors & mutators
+        int getDims() const { return dim; }
+        void setValue(int, double);
+        double getValue(int) const;
+
+        // Functions
+        double distanceTo(const Point &) const;
+
+        // Overloaded operators
+
+        // Members
+        Point &operator*=(double);
+        Point &operator/=(double);
+        const Point operator*(double) const; // prevent (p1*2) = p2;
+        const Point operator/(double) const;
+
+        double &operator[](int index) { return values[index - 1]; } // TODO out-of-bds?
+
+        // Friends
+        friend Point &operator+=(Point &, const Point &);
+        friend Point &operator-=(Point &, const Point &);
+        friend const Point operator+(const Point &, const Point &);
+        friend const Point operator-(const Point &, const Point &);
+
+        friend bool operator==(const Point &, const Point &);
+        friend bool operator!=(const Point &, const Point &);
+
+        friend bool operator<(const Point &, const Point &);
+        friend bool operator>(const Point &, const Point &);
+        friend bool operator<=(const Point &, const Point &);
+        friend bool operator>=(const Point &, const Point &);
+
+        friend std::ostream &operator<<(std::ostream &, const Point &);
+        friend std::istream &operator>>(std::istream &, Point &);
+    };
+
+}
+
+#endif //CSI2312_PA2_POINT_H
