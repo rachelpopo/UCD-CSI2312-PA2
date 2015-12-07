187bfeaf305eaa215ef300baec5a7cd07ffac6e2
Rachel Popo <rach.popo@gmail.com>
2015-12-07 10:49:28 -0700

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,6 +1,5 @@
-
 //
-// Created by Rachel Popo on 9/21/15.
+// Created by Rachel Popo
 //
 
 #include "Point.h"
@@ -8,119 +7,190 @@
 #include <cmath>
 #include <iostream>
 
-namespace Clustering
-{
+namespace Clustering {
     //Point member functions
-    Point::Point()
-    {
+    Point::Point() {
+        dim = 0;
+        values = NULL;
+
     }
 
-    Point::Point(int newDim)
-    {
-        dim = newDim;
-    }
-
-    Point::Point(int newDim, double *newValues)
-    {
+    Point::Point(int newDim) {
         dim = newDim;
         values = new double[dim];
-        for (int i = 0; i < dim; ++i)
-        {
+        for (int i = 0; i < dim; ++i) {
+            values[i] = 0;
+        }
+
+    }
+
+    Point::Point(int newDim, double *newValues) {
+        dim = newDim;
+        values = new double[dim];
+
+        for (int i = 0; i < dim; ++i) {
             values[i] = newValues[i];
         }
+
     }
 
     // Big three: cpy ctor, overloaded operator=, dtor
-    Point::Point(const Point &toCopy)
-    {
-        dim = toCopy.dim;
+    Point::Point(const Point &point) {
+        dim = point.dim;
         values = new double[dim];
 
-        for (int i = 0; i < dim; ++i)
-            values[i] = toCopy.values[i];
+        for (int i = 0; i < dim; ++i) {
+            values[i] = point.values[i];
+        }
     }
 
-    Point& Point::operator=(const Point &toAssign)
-    {
-        if (toAssign == *this)
-              return *this;
+    Point &Point::operator=(const Point &point) {
+        if (this == &point) {
+            return *this;
+        }
 
-        dim = toAssign.dim;
+        if (values != NULL) {
+            delete values;
+        }
+
+        dim = point.dim;
         values = new double[dim];
 
-        for (int i = 0; i < dim; ++i)
-            values[i] = toAssign.values[i];
+        for (int i = 0; i < dim; ++i) {
+            values[i] = point.values[i];
+        }
 
         return *this;
     }
 
-    Point::~Point()
-    {
-        delete [] values;
+    Point::~Point() {
+        delete values;
     }
 
-    Point& Point::operator *= (double n)
-    {
-        for (int i = 0; i < dim; ++i)
+    Point &Point::operator*=(double n) {
+        for (int i = 0; i < dim; ++i) {
             values[i] *= n;
+        }
 
         return *this;
     }
 
-    Point& Point::operator /= (double n)
-    {
-        for (int i = 0; i < dim; ++i)
+    Point &Point::operator/=(double n) {
+        if (n == 0) {
+            std::cout << "Can not divide by zero." << std::endl;
+            return *this;
+        }
+
+        for (int i = 0; i < dim; ++i) {
             values[i] /= n;
+        }
 
         return *this;
     }
 
-    const Point Point::operator*(double n) const
-    {
+    const Point Point::operator*(double n) const {
         Point a(dim);
-        for (int i = 0; i < dim; ++i)
+        for (int i = 0; i < dim; ++i) {
             a.values[i] = values[i] * n;
+        }
 
         return a;
 
     }
 
-    const Point Point::operator/(double n) const
-    {
-        Point a(dim);
-        for (int i = 0; i < dim; ++i)
-            a.values[i] = values[i] / n;
-
-        return a;
-    }
-
-
-    void Point::setValue(int index, double aDouble)
-    {
-        if (index >= 0 && index < dim)
-            values [index] = aDouble;
-    }
-
-    double Point::getValue(int index) const
-    {
-        if (index >= 0 && index < dim)
-            return values[index];
-        else
+    const Point Point::operator/(double n) const {
+        if (n == 0) {
+            std::cout << "Can not divide by zero." << std::endl;
             return 0;
+        }
+
+        Point a(dim);
+        for (int i = 0; i < dim; ++i) {
+            a.values[i] = values[i] / n;
+        }
+
+        return a;
     }
 
-    double Point::distanceTo(const Point &aPoint) const
-    {
-        if (aPoint.dim == dim)
-        {
+
+    //Sort function
+    void lSort(int dim, double *values){
+        double temp;
+
+        for (int i = 0; i < dim - 1; i++){
+
+            if (values[i] > values [i + 1]){
+                temp = values [i];
+                values [i] = values [i + 1];
+                values [i + 1] = temp;
+            }
+        }
+        return;
+    }
+
+
+    void Point::setValue(int index, double n) {
+        if (index >= 0 && index < dim) {
+            values[index] = n;
+        }
+    }
+
+    double Point::getValue(int index) const {
+        if (index >= 0 && index < dim) {
+            return values[index];
+        }
+
+        else {
+            return 0;
+        }
+    }
+
+    void Point::clear () {
+        if (values != NULL) {
+            delete values;
+        }
+        values = NULL;
+        dim = 0;
+    }
+
+
+    void Point::append(double v){
+        if (values == NULL){
+            dim = 1;
+            values = new double [dim];
+            values[0] = v;
+            return;
+        }
+
+        dim++;
+        double *values2 = new double [dim];
+
+        for (int i = 0; i < dim - 1; i++){
+            values2[i] = values[i];
+        }
+
+        values2[dim-1] = v;
+        delete values;
+        values = values2;
+        return;
+
+    }
+
+
+    double Point::distanceTo(const Point &point) const {
+        if (point.dim == dim) {
             double sum = 0;
-            for (int i = 0; i < dim; ++i)
-                sum += ((aPoint.values[i] - values[i]) * (aPoint.values[i] - values[i]));
+            for (int i = 0; i < dim; ++i) {
+                double temp = point.values[i] - values[i];
+                sum += temp * temp;
+            }
 
             return sqrt(sum);
         }
-        else
+
+        else {
             return 0;
+        }
     }
 
     /////////////////////////////
@@ -128,22 +198,19 @@ namespace Clustering
     //Notice they don't need to have te class specififer Point:: infront
     //because they are not member functions of class Point
 
-    Point& operator+=(Point &a, const Point &b)
-    {
+    Point &operator+=(Point &a, const Point &b) {
         for (int i = 0; i < a.dim; ++i)
             a.values[i] += b.values[i];
         return a;
     }
 
-    Point& operator-=(Point &a, const Point &b)
-    {
+    Point &operator-=(Point &a, const Point &b) {
         for (int i = 0; i < a.dim; ++i)
             a.values[i] -= b.values[i];
         return a;
     }
 
-    const Point operator+(const Point &a, const Point &b)
-    {
+    const Point operator+(const Point &a, const Point &b) {
         const Point temp(a.dim);
         for (int i = 0; i < a.dim; ++i)
             temp.values[i] = a.values[i] + b.values[i];
@@ -151,8 +218,7 @@ namespace Clustering
         return temp;
     }
 
-    const Point operator-(const Point &a, const Point &b)
-    {
+    const Point operator-(const Point &a, const Point &b) {
         const Point temp(a.dim);
         for (int i = 0; i < a.dim; ++i)
             temp.values[i] = a.values[i] - b.values[i];
@@ -160,12 +226,10 @@ namespace Clustering
         return temp;
     }
 
-    bool operator==(const Point &a, const Point &b)
-    {
+    bool operator==(const Point &a, const Point &b) {
         bool compare = true;
 
-        for (int i = 0; i < a.dim; ++i)
-        {
+        for (int i = 0; i < a.dim; ++i) {
             if (a.values[i] != b.values[i])
                 compare = false;
         }
@@ -173,12 +237,10 @@ namespace Clustering
         return compare;
     }
 
-    bool operator!=(const Point &a, const Point &b)
-    {
+    bool operator!=(const Point &a, const Point &b) {
         bool compare = false;
 
-        for (int i = 0; i < a.dim; ++i)
-        {
+        for (int i = 0; i < a.dim; ++i) {
             if (a.values[i] != b.values[i])
                 compare = true;
         }
@@ -186,12 +248,10 @@ namespace Clustering
         return compare;
     }
 
-    bool operator<(const Point &a, const Point &b)
-    {
+    bool operator<(const Point &a, const Point &b) {
         bool compare = false;
 
-        for (int i = 0; i < a.dim; ++i)
-        {
+        for (int i = 0; i < a.dim && i < b.dim; ++i) {
             if (a.values[i] < b.values[i])
                 compare = true;
         }
@@ -199,12 +259,10 @@ namespace Clustering
         return compare;
     }
 
-    bool operator>(const Point &a, const Point &b)
-    {
+    bool operator>(const Point &a, const Point &b) {
         bool compare = false;
 
-        for (int i = 0; i < a.dim; ++i)
-        {
+        for (int i = 0; i < a.dim; ++i) {
             if (a.values[i] > b.values[i])
                 compare = true;
         }
@@ -212,12 +270,10 @@ namespace Clustering
         return compare;
     }
 
-    bool operator<=(const Point &a, const Point &b)
-    {
+    bool operator<=(const Point &a, const Point &b) {
         bool compare = false;
 
-        for (int i = 0; i < a.dim; ++i)
-        {
+        for (int i = 0; i < a.dim; ++i) {
             if (a.values[i] <= b.values[i])
                 compare = true;
         }
@@ -225,12 +281,10 @@ namespace Clustering
         return compare;
     }
 
-    bool operator>=(const Point &a, const Point &b)
-    {
+    bool operator>=(const Point &a, const Point &b) {
         bool compare = false;
 
-        for (int i = 0; i < a.dim; ++i)
-        {
+        for (int i = 0; i < a.dim; ++i) {
             if (a.values[i] >= b.values[i])
                 compare = true;
         }
@@ -238,24 +292,47 @@ namespace Clustering
         return compare;
     }
 
- std::ostream &operator<<(std::ostream &out, const Point &p)
-   {
-   //     for (int i = 0; i < p.dim - 1; ++i)
-   //         out << p.values[i] << ", ";
-   //     out << p.values[p.dim - 1]; //prints our the last value without a comma at the end
-   //     out << std::endl; //print new line
-   //     return out;
+
+    std::ostream &operator<<(std::ostream &out, const Point &point) {
+
+        out << "(";
+
+        for (int i = 0; i < point.dim; ++i) {
+            if (i < point.dim - 1) {
+                out << point.values[i] << ",";
+            }
+            else {
+                out << point.values[i];
+            }
+        }
+
+        out << ")";
+
+        out << std::endl; //print new line
+
+        return out;
+
     }
 
-    std::istream &operator>>(std::istream &in, Point &p)
-    {
 
-      //  for (int i = 0; i < p.dim - 1; ++i)
-        //    in >> p.values[i] >> ", ";
-        //in >> p.values[p.dim - 1]; //prints our the last value without a comma at the end
-        //in >> std::endl; //print new line
+    std::istream &operator>>(std::istream &in, Point &point) {
 
-        //return in;
+        point.clear();
+
+       char openParen;
+       in >> openParen;
+
+       double value;
+       char c = in.peek();
+
+       while (c != ')'){
+           in >> value;
+           point.append(value);
+           in >> c;
+       }
+
+        return in;
     }
 
 }
+
