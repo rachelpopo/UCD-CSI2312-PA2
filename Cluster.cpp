f7714eda3c69939fc90e5b5b6b5ab7dbaf1ae26d
Rachel Popo <rach.popo@gmail.com>
2015-12-16 15:27:35 -0700

    Version 3.0
    changes made for kmeans

--- Cluster.cpp
+++ Cluster.cpp
@@ -1,3 +1,4 @@
+$ git tag -1 "version 3.0"
 $ git tag -1 "version 2.0"
 
 //
@@ -19,6 +20,9 @@ namespace Clustering {
     Cluster::Cluster(const Cluster &cluster) { //copy reference toCopy's values into our clusters values
         __size = cluster.__size;
         __points = NULL;
+        valid = cluster.valid;
+        __ID = cluster.__ID;
+        __centroid = cluster.__centroid;
 
         LNodePtr last = NULL;
 
@@ -44,6 +48,9 @@ namespace Clustering {
             return *this;
         }
 
+        __centroid=cluster.__centroid;
+        valid = cluster.valid;
+        __ID = cluster.__ID;
         __size = cluster.__size;
         __points = NULL;
 
@@ -94,6 +101,7 @@ namespace Clustering {
             __size++;
             LNodePtr node = new LNode(point, NULL);
             __points = node;
+            valid=false;
             return;
         }
 
@@ -102,6 +110,7 @@ namespace Clustering {
             LNodePtr node = new LNode(point, NULL);
             node->next = __points;
             __points = node;
+            valid=false;
             return;
         }
 
@@ -124,6 +133,8 @@ namespace Clustering {
         LNodePtr node = new LNode(point, NULL);
         prev->next = node;
         node->next = p;
+
+        valid=false;
         return;
     }
 
@@ -138,6 +149,7 @@ namespace Clustering {
             __points = p->next;
             delete p;
             __size--;
+            valid = false;
             return point;
 
         }
@@ -148,11 +160,12 @@ namespace Clustering {
                 prev->next = p->next;
                 delete p;
                 __size--;
+                valid = false;
                 return point;
             }
 
         }
-
+        valid = false;
         return point;
 
     }
@@ -198,6 +211,8 @@ namespace Clustering {
         LNodePtr node = new LNode(rhs, NULL);
         prev->next = node;
         node->next = p;
+
+        valid = false;
         return *this;
 
     }
@@ -233,7 +248,7 @@ namespace Clustering {
             }
 
         }
-
+        valid = false;
         return *this;
     }
 
@@ -282,7 +297,7 @@ namespace Clustering {
             prev = node;
         }
 
-
+        valid = false;
         return *this;
 
 
@@ -320,7 +335,7 @@ namespace Clustering {
             }
 
         }
-
+        valid = false;
         return *this;
 
     }
@@ -330,7 +345,10 @@ namespace Clustering {
 ///////////////////////////////
 
 //// IO
-    std::ostream &operator<<(std::ostream &out, const Cluster &cluster) {
+    std::ostream &operator<<(std::ostream &out, const Cluster &cluster) {       /*should put on the stream all its points with its id at the end of each point line,
+                                                                               delimited by colon (':') from the Point values. Note: operator<< for Point and Cluster
+                                                                               have to be compatible for this to work. Use a static constant for the delimiter character.
+                                                                               See the file format snippets above.*/
 
         LNodePtr c = cluster.__points;
 
@@ -346,7 +364,11 @@ namespace Clustering {
         return out;
     }
 
-    std::istream &operator>>(std::istream &in, Cluster &cluster) {
+    std::istream &operator>>(std::istream &in, Cluster &cluster) {          /*should use getline on the stream, and pass a stringstream for each line to the
+                                                                           operator>> of Point, thus reading in all the points into itself. Note 1: operator>>
+                                                                           for Point and Cluster have to be compatible for this to work. Use the basic IO code we
+                                                                           saw in class to accomplish this. Note 2: This operator also has to discover the dimensionality
+                                                                           of the points that are being read in. */
 
         return in;
     }
@@ -377,10 +399,6 @@ namespace Clustering {
 
            compare = true;
            return compare;
-
-
-
-        return compare;
     }
 
 //// Set-destructive operators (duplicate points in the space)
@@ -479,7 +497,7 @@ namespace Clustering {
 
         if (c->__points == NULL){
             c->__size++;
-            LNodePtr node = new LNode (rhs, NULL);
+            LNodePtr node = new LNode (new Point(*rhs), NULL);
             c->__points = node;
             return *c;
         }
@@ -509,6 +527,7 @@ namespace Clustering {
         LNodePtr node = new LNode(rhs, NULL);
         prev->next = node;
         node->next = p;
+        c->valid=false;
         return *c;
 
     }
@@ -548,8 +567,151 @@ namespace Clustering {
 
         }
 
+        c->valid = false;
         return *c;
 
+    }
+
+
+
+
+///CENTROID FUNCTIONS
+
+
+    const Point Cluster::computeCentroid(const Point &point){
+
+        if (__points==NULL){
+            return 0;
+        }
+
+        Point *average = new Point(__points->p->getDim());
+        double total = 0;
+
+        for (LNodePtr p = __points; p != NULL; p = p->next){
+            *average = *average + *p->p;
+            total++;
+        }
+
+        *average = *average / total;
+
+        __centroid = *average;
+        valid = true;
+
+        return *average;
+
+    }
+
+
+
+    //Pick k points
+    void Cluster::pickPoints(int k, PointPtr *pointArray) {
+
+       if (__points==NULL || k == 0){
+           return;
+       }
+
+       if (__size <= k){
+            int i = 0;
+
+            for (LNodePtr p = __points; p != NULL; p = p->next ){
+
+                *pointArray[i] = *p->p;
+                i++;
+            }
+           return;
+       }
+
+        int spread = __size / k;
+        int count = 1;
+        int i = 0;
+
+        for (LNodePtr p = __points; p!=NULL; p = p->next){
+            if (count != spread){
+                count++;
+                continue;
+            }
+
+            else {
+                *pointArray[i] = *p->p;
+                i++;
+                count = 1;
+                continue;
+            }
+        }
+
+
+    }
+
+    //Distance between every 2 pts in cluster
+    double Cluster::intraClusterDistance() const{
+
+      double sum = 0.0;
+
+       if (__points == NULL){
+           return sum;
+       }
+
+        LNodePtr p2 = __points;
+
+       for (LNodePtr p1 = __points; p2!=NULL; p1=p1->next){
+           p2 = p1->next;
+           sum = sum + p1->p->distanceTo(*p2->p);
+       }
+        return sum;
+    }
+
+
+    //Distance between 2 clusters
+    double interClusterDistance(const Cluster &c1, const Cluster &c2){
+
+        double distance = 0.0;
+
+        if (c1.__points == NULL || c2.__points == NULL){
+            return distance;
+        }
+
+
+        LNodePtr p2 = c2.__points;
+
+        for (LNodePtr p1 = c1.__points; p1 != NULL && p2 != NULL; p1 = p1->next) {
+            distance = distance + p1->p->distanceTo(*p2->p);
+            p2 = p2->next;
+        }
+
+        return distance;
+
+    }
+
+    //Edges between 2 clusters
+    double interClusterEdges(const Cluster &c1, const Cluster &c2){
+        double distance = 0.0;
+
+        if (c1.__points == NULL || c2.__points == NULL){
+            return distance;
+        }
+
+        distance = c1.__size * c2.__size;
+        return distance;
+
+    }
+
+
+    //Get Edges of cluster
+    int Cluster::getClusterEdges(){
+        int distance;
+        distance = (__size - 1) * (__size / 2);
+        return distance;
+    }
+
+
+    //Move Function
+    void Cluster::Move::perform() {
+        if (fromCluster->__points==NULL){
+            return;
+        }
+
+        fromCluster->remove(point);
+        toCluster->add(point);
     }
 
 }
