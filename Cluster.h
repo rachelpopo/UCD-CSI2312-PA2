68b1e22ec09e6530ccda8cb095a2821fd8325190
Rachel Popo <rach.popo@gmail.com>
2015-12-16 15:29:27 -0700

    Version 3.0
    changes made to implement kmeans

--- Cluster.h
+++ Cluster.h
@@ -1,3 +1,4 @@
+$ git tag "version 3.0"
 $ git tag "version 2.0"
 
 //
@@ -19,31 +20,56 @@ namespace Clustering {
     };
 
     class Cluster {
+        unsigned int __ID;
         int __size;
         LNodePtr __points;
-        bool __release_points;
         bool __greedy;
+        Point __centroid;
+        bool valid;
 
         void __del();
         void __cpy(LNodePtr pts);
 
     public:
+        class Move{                                                             //represents motion of pt from one cluster to another
+            Cluster* fromCluster;
+            Cluster* toCluster;
+            PointPtr point;
+
+            Move(const PointPtr &ptr, Cluster *from, Cluster *to):
+                    fromCluster(from),
+                    toCluster(to),
+                    point(ptr)
+            {};
+
+            void perform();
+        };
+
+
+
         Cluster() :
+                __ID(generateID()),
                 __size(0),
                 __points(nullptr),
-                __release_points(false),
+                __centroid(),
+                valid(false),
                 __greedy(false)
         {};
+
         Cluster(bool rp) :
+                __ID(generateID()),
                 __size(0),
                 __points(nullptr),
-                __release_points(rp),
+                __centroid(),
+                valid(false),
                 __greedy(true)
         {};
         Cluster(bool rp, bool gdy) :
+                __ID(generateID()),
                 __size(0),
                 __points(nullptr),
-                __release_points(rp),
+                __centroid(),
+                valid(false),
                 __greedy(gdy)
         {};
 
@@ -55,6 +81,16 @@ namespace Clustering {
         // Set functions: They allow calling c1.add(c2.remove(p));
         void add(const PointPtr &);
         const PointPtr &remove(const PointPtr &);
+
+        static unsigned int generateID(){
+                static unsigned int i = 0;
+                ++i;
+                return i;
+        }
+
+        static unsigned int getID(Cluster &cluster){
+            return cluster.__ID;
+        }
 
         // Overloaded operators
 
@@ -76,6 +112,8 @@ namespace Clustering {
         Cluster &operator+=(const Cluster &rhs); // union
         Cluster &operator-=(const Cluster &rhs); // (asymmetric) difference
 
+
+
         // - Friends
 
 
@@ -85,6 +123,17 @@ namespace Clustering {
         friend const Cluster operator+(const Cluster &lhs, const PointPtr &rhs);
         friend const Cluster operator-(const Cluster &lhs, const PointPtr &rhs);
 
+
+
+        //Methods for scoring the clustering (used in requirements for KMeans)
+        friend double interClusterDistance(const Cluster &c1, const Cluster &c2);
+        friend double interClusterEdges(const Cluster &c1, const Cluster &c2);
+
+        double intraClusterDistance() const;
+        int getClusterEdges();
+
+
+
         LNodePtr getPoints(){
             return __points;
         }
@@ -92,6 +141,24 @@ namespace Clustering {
         int getSize(){
             return __size;
         }
+
+        //Centroid Functions
+        void setCentroid(const Point &point){
+            __centroid = point;
+            valid = true;
+        };
+        const Point getCentroid(){return __centroid;};
+        const Point computeCentroid(const Point &point);
+        bool isCentroidValid(){ return valid;};
+
+
+        //Pick points to fill array
+        void pickPoints(int k, PointPtr *pointArray);
+
+
+
+
+
     };
 
 }
